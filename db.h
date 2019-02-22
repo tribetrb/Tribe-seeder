cddb9d8b4a47e974463704cf93cc2381b42ec146
tribetrb <47125908+tribetrb@users.noreply.github.com>
2019-02-22 01:11:29 -0600

    Update db.h

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 1000 : 170201;
+    return testnet ? 1000 : 260000;
 }
 
 std::string static inline ToString(const CService &ip) {
